5ca455450630b1748b3fed0fc31928cab76e5059
wang-sai <532969033@qq.com>
2018-05-28 23:46:41 +0800

    :lipstick: update index.vue css

--- client/src/pages/index/index.vue
+++ client/src/pages/index/index.vue
@@ -1,18 +1,14 @@
 <template>
   <y-layout menu="index">
     <div class="content-home">
-      <div class="content-home-title text-shadow">
-        <div class="content-home-tip text-shadow" :class="{'content-home-switch-tip': switchTip}">{{ tip }}</div>
-      </div>
-      <!-- <div class="content-home-cover" :style="'background-image: url(' + imageUrl + ');'"></div> -->
-      <div class="content-home-cover"></div>
+      <div class="content-home-tip text-shadow" :class="{'content-home-switch-tip': switchTip}">{{ tip }}</div>
+      <div class="content-home-cover" :style="'background-image: url(' + imageUrl + ');'"></div>
     </div>
   </y-layout>
 </template>
 
 <script>
 import YLayout from 'components/layout/layout'
-// import { dateFormat } from 'common/js/util'
 import CONST from 'api/const'
 
 const tips = CONST.tips || []
@@ -35,20 +31,17 @@ export default {
     initData () {
       this.initBgImage()
       this.sayAWord()
-      setInterval(() => {
-        this.sayAWord()
-      }, 10000)
+      setInterval(this.sayAWord, 5000)
     },
     sayAWord () {
       this.switchTip = true
       setTimeout(() => {
         this.tip = `“${tips[Math.random() * tips.length >> 0]}”`
         this.switchTip = false
-      }, 1500)
+      }, 500)
     },
     initBgImage () {
-      // let time = dateFormat(null, 'yyyyMMdd')
-      // this.imageUrl = `http://cn.bing.com/iod/1366/1024/${time}1600`
+      this.imageUrl = 'https://source.unsplash.com/random'
     }
   }
 }
@@ -58,51 +51,33 @@ export default {
 .content-home
   position: relative
   height: 100%
-  .content-home-title
-    position: absolute
-    color: #fff
-    z-index: 1
-    width: 40%
-    text-align: center
-    height: 0.8rem
-    top: 3rem
-    margin-top: -1rem
-    transform: translate(-50%)
-    left: 50%
-    background-color: rgba(0,0,0,0.6)
-    padding: 0.1rem 0
-    h3
-      font-size: 0.22rem
-      letter-spacing: 4px
-      line-height: 0.3rem
   .content-home-tip
     position: absolute
-    width: 80%
     left: 50%
     top: 40%
-    transform: translate(-50%)
-    color: #fff
-    text-align: center
-    font-size: 0.19rem
-    line-height: 1.5
-    transition: all .6s ease
     z-index: 1
+    font-size: 0.19rem
+    text-align: center
+    line-height: 1.5
+    padding: .1rem .2rem
+    color: #ffffff
+    background: #000000
+    background-color: rgba(0, 0, 0, 0.6)
+    transform: translate(-50%)
+    transition: all .6s ease
     &.content-home-switch-tip
       opacity: 0
-      transform: translate(-50%,0%) translateY(10px)
+      transform: translate(-50%, 0%) translateY(10px)
   .content-home-cover
-    background-image: url(./image/banner1.jpg)
+    // background-image: url(./image/banner1.jpg)
     background-position: center center
     background-repeat: no-repeat
-    // filter: grayscale(100%)
     background-size: cover
-    // height: 100%
-    height: 5.3rem
+    height: 80%
     position: relative
-    top: 1.2rem
+    top: 10%
     &:before
       content: ''
-      display: block
       position: absolute
       left: 0
       top: 0
@@ -112,6 +87,6 @@ export default {
       background-image: url(./image/mask.png)
 @media (max-width: 992px)
   .content-home
-    .content-home-title
-      width: 80%
+    .content-home-cover
+      top: 15%
 </style>
